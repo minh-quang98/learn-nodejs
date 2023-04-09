f74362c25d9f29b36e48937cefcd8f9d33b007e2
Nguyen Minh Quang <minhquang10998@gmail.com>
2023-04-09 14:48:24 +0700

    update befor mvc

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true })); // if false show object prototype instead
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminData.router);
-
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render('404', {pageTitle: "Page Not Found"})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
